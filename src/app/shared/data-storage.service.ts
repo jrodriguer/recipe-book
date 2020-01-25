af85d2e714cf6cd2a6a1ece353e8598e5b2d0a41
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2020-01-25 04:02:48 +0100

    WIP Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { RecipeService } from '../recipes/recipe.service';
+
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private recipeSrv: RecipeService) {}
+
+    storageRecipes() {
+        const recipes = this.recipeSrv.getRecipes();
+        return this.http
+            .put(
+                'https://ng-project-recipesfood.firebaseio.com/recipes.json',
+                recipes,
+            )
+            .subscribe(resp => {
+                console.log(resp);
+            });
+    }
 }
