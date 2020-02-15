2e4fc7874f97a3c2015e64c1875a0cd77f605295
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2020-02-15 17:54:26 +0100

    WIP

---
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-loading-spinner',
+    templateUrl: '<div class="loader">Loading...</div>',
+    styleUrls: ['./app.component.css'],
+})
+export class LoadingSpinner {}
