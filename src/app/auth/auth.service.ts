7b27fa2691a8aa203e40f280022bd66a3ab8d746
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2020-02-16 07:32:55 +0100

    WIP

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Observable, throwError } from "rxjs";
-import { catchError } from "rxjs/operators";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 interface AuthResponseData {
     kind: string;
@@ -9,9 +9,10 @@ interface AuthResponseData {
     email: string;
     refreshToken: string;
     expiresIn: string;
+    registered?: boolean;
 }
 
-@Injectable({ providedIn: "root" })
+@Injectable({ providedIn: 'root' })
 export class AuthService {
     constructor(private http: HttpClient) {}
 
@@ -19,28 +20,39 @@ export class AuthService {
         return (
             this.http
                 .post<AuthResponseData>(
-                    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB0V4q7fRyp_Vs8MpykxGPoZGS3WmVdsVg",
+                    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB0V4q7fRyp_Vs8MpykxGPoZGS3WmVdsVg',
                     {
                         email: email,
                         password: password,
-                        returnSecureToken: true
-                    }
+                        returnSecureToken: true,
+                    },
                 )
                 // Error message conversion logic
                 //
                 .pipe(
                     catchError(errRes => {
-                        let errorMessg = "An unknown error ocurred";
+                        let errorMessg = 'An unknown error ocurred';
                         if (!errRes.error || !errRes.error.error) {
                             return throwError(errorMessg);
                         }
                         switch (errRes.error.error.message) {
-                            case "EMAIL_EXISTS":
-                                errorMessg = "This exists already";
+                            case 'EMAIL_EXISTS':
+                                errorMessg = 'This exists already';
                         }
                         return throwError(errorMessg);
-                    })
+                    }),
                 )
+        );
+    }
+
+    signIn(email: string, pw: string) {
+        this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB0V4q7fRyp_Vs8MpykxGPoZGS3WmVdsVg',
+            {
+                email: email,
+                password: pw,
+                returnSecureToken: true,
+            },
         );
     }
 }
