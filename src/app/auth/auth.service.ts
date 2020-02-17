75d639cb2811780d4a77b088e861fdcea675f4c8
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2020-02-17 01:13:17 +0100

    Creating & Storing the User Data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Observable, throwError, Subject } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+import { User } from './user.model';
 
 export interface AuthResponseData {
     kind: string;
     idToken: string;
+    localId: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
@@ -14,35 +17,31 @@ export interface AuthResponseData {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+    public user = new Subject<User>(); // store user state
+
     constructor(private http: HttpClient) {}
 
     signUp(email: string, password: string): Observable<AuthResponseData> {
-        return (
-            this.http
-                .post<AuthResponseData>(
-                    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB0V4q7fRyp_Vs8MpykxGPoZGS3WmVdsVg',
-                    {
-                        email: email,
-                        password: password,
-                        returnSecureToken: true,
-                    },
-                )
-                // Error message conversion logic
-                //
-                .pipe(
-                    catchError(errRes => {
-                        let errorMessg = 'An unknown error ocurred';
-                        if (!errRes.error || !errRes.error.error) {
-                            return throwError(errorMessg);
-                        }
-                        switch (errRes.error.error.message) {
-                            case 'EMAIL_EXISTS':
-                                errorMessg = 'This exists already';
-                        }
-                        return throwError(errorMessg);
-                    }),
-                )
-        );
+        return this.http
+            .post<AuthResponseData>(
+                'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB0V4q7fRyp_Vs8MpykxGPoZGS3WmVdsVg',
+                {
+                    email: email,
+                    password: password,
+                    returnSecureToken: true,
+                },
+            )
+            .pipe(
+                catchError(this.handleError),
+                tap(resData => {
+                    this.handleAuth(
+                        resData.email,
+                        resData.expiresIn,
+                        resData.idToken,
+                        +resData.expiresIn,
+                    );
+                }),
+            );
     }
 
     signIn(email: string, pw: string): Observable<AuthResponseData> {
@@ -57,6 +56,19 @@ export class AuthService {
             )
 
             .pipe(catchError(this.handleError));
+    }
+
+    private handleAuth(
+        email: string,
+        userId: string,
+        token: string,
+        expiresIn: number,
+    ) {
+        const expirationDate = new Date(
+            new Date().getTime() + expiresIn * 1000,
+        );
+        const user = new User(email, userId, token, expirationDate);
+        this.user.next(user);
     }
 
     private handleError(errRes: HttpErrorResponse) {
