352827a61bf44595fea41531418cd7b64f61725d
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2020-02-29 19:45:45 +0100

    Refactor Adding Logout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import { Subscription } from "rxjs";
+import { Subscription } from 'rxjs';
 
-import { DataStorageService } from "../shared/data-storage.service";
-import { AuthService } from "../auth/auth.service";
+import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
-    selector: "app-header",
-    templateUrl: "./header.component.html"
+    selector: 'app-header',
+    templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit, OnDestroy {
     private userSub: Subscription;
@@ -15,13 +15,17 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
     constructor(
         private dataStorageSrv: DataStorageService,
-        private authSrv: AuthService
+        private authSrv: AuthService,
     ) {}
 
     ngOnInit() {
         this.userSub = this.authSrv.user.subscribe(user => {
             this.isAuthenticated = !!user; // !user ? false : true
         });
+    }
+
+    onLogout() {
+        this.authSrv.logout();
     }
 
     onSaveData() {
