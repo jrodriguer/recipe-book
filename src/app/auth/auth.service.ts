b8067674a977559b370c40b96bbcd783f0d198e7
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2020-06-28 20:15:22 +0200

    Refactor style code

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Router } from "@angular/router";
 
-import { Observable, throwError, BehaviorSubject } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { Observable, throwError, BehaviorSubject } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
 
-import { User } from '../../models/user.model';
-import { AuthResponseData } from 'src/models/auth-model.temp';
-import { environment } from '../../environments/environment';
+import { User } from "../../models/user.model";
+import { AuthResponseData } from "src/models/auth-model.temp";
+import { environment } from "../../environments/environment";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class AuthService {
     public user = new BehaviorSubject<User>(null); // store and info user state
     public tokenExpirationTimer: any;
@@ -19,13 +19,13 @@ export class AuthService {
     signUp(email: string, password: string): Observable<AuthResponseData> {
         return this.http
             .post<AuthResponseData>(
-                'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+                "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" +
                     environment.firebaseAPIKey,
                 {
                     email: email,
                     password: password,
-                    returnSecureToken: true,
-                },
+                    returnSecureToken: true
+                }
             )
             .pipe(
                 catchError(this.handleError),
@@ -34,22 +34,22 @@ export class AuthService {
                         resData.email,
                         resData.expiresIn,
                         resData.idToken,
-                        +resData.expiresIn, // treated as number
+                        +resData.expiresIn // treated as number
                     );
-                }),
+                })
             );
     }
 
     signIn(email: string, pw: string): Observable<AuthResponseData> {
         return this.http
             .post<AuthResponseData>(
-                'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
+                "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" +
                     environment.firebaseAPIKey,
                 {
                     email: email,
                     password: pw,
-                    returnSecureToken: true,
-                },
+                    returnSecureToken: true
+                }
             )
             .pipe(
                 catchError(this.handleError),
@@ -58,9 +58,9 @@ export class AuthService {
                         resData.email,
                         resData.expiresIn,
                         resData.idToken,
-                        +resData.expiresIn,
+                        +resData.expiresIn
                     );
-                }),
+                })
             );
     }
 
@@ -70,7 +70,7 @@ export class AuthService {
             id: string;
             _token: string;
             _tokenExpirationDate: string;
-        } = JSON.parse(localStorage.getItem('userData'));
+        } = JSON.parse(localStorage.getItem("userData"));
         if (!userData) {
             return;
         }
@@ -79,7 +79,7 @@ export class AuthService {
             userData.email,
             userData.id,
             userData._token,
-            new Date(userData._tokenExpirationDate),
+            new Date(userData._tokenExpirationDate)
         );
         if (loaderUser.token) {
             this.user.next(loaderUser);
@@ -92,8 +92,8 @@ export class AuthService {
 
     logout() {
         this.user.next(null);
-        this.router.navigate(['/auth']);
-        localStorage.removeItem('userData');
+        this.router.navigate(["/auth"]);
+        localStorage.removeItem("userData");
         if (this.tokenExpirationTimer) {
             clearTimeout(this.tokenExpirationTimer);
         }
@@ -120,31 +120,31 @@ export class AuthService {
         email: string,
         userId: string,
         token: string,
-        expiresIn: number,
+        expiresIn: number
     ) {
         const expirationDate = new Date(
-            new Date().getTime() + expiresIn * 1000,
+            new Date().getTime() + expiresIn * 1000
         );
         const user = new User(email, userId, token, expirationDate);
         this.user.next(user);
         this.autoLogout(expiresIn * 1000);
-        localStorage.setItem('userData', JSON.stringify(user));
+        localStorage.setItem("userData", JSON.stringify(user));
     }
 
     private handleError(errRes: HttpErrorResponse): Observable<never> {
-        let errorMessg = 'An unknown error ocurred';
+        let errorMessg = "An unknown error ocurred";
         if (!errRes.error || !errRes.error.error) {
             return throwError(errorMessg);
         }
         switch (errRes.error.error.message) {
-            case 'EMAIL_EXISTS':
-                errorMessg = 'This email exists already';
+            case "EMAIL_EXISTS":
+                errorMessg = "This email exists already";
                 break;
-            case 'EMAIL_NOT_FOUND':
-                errorMessg = 'This email does not exist';
+            case "EMAIL_NOT_FOUND":
+                errorMessg = "This email does not exist";
                 break;
-            case 'INVALID_PASSWORD':
-                errorMessg = 'This password is not correct';
+            case "INVALID_PASSWORD":
+                errorMessg = "This password is not correct";
                 break;
         }
         return throwError(errorMessg);
