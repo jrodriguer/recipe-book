e12272b234c19da872270229458de11abab1a91f
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2021-07-02 10:38:51 +0200

    Adding Actions

--- src/app/pages/shopping-list/shopping-list.reducer.ts
+++
@@ -1,7 +0,0 @@
-import { Ingredient } from '../../../models/ingredient.model';
-
-const initialState = {
-    ingredient: [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)],
-};
-
-export function shoppingListReducer(state = initialState, action) {}
