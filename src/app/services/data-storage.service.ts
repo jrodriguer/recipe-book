4afea5fb6ab51522db1843edf03e3b180f524356
Julio <46267103+jrodriguer@users.noreply.github.com>
2023-03-18 15:56:09 +0100

    fix realtime db url

--- src/app/services/data-storage.service.ts
+++ src/app/services/data-storage.service.ts
@@ -1,31 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
+import { doc } from 'firebase/firestore';
 
 import { RecipeService } from './recipe.service';
 import { Recipe } from '../../models/recipe.model';
+import { Ingredient } from '../../models/ingredient.model';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
   constructor(private http: HttpClient, private recipeSrv: RecipeService) {}
 
-  storageRecipes(): Subscription {
+  storageRecipes(): Observable<Object> {
     const recipes = this.recipeSrv.getRecipes();
-    return this.http
-      .put(
-        'https://ng-project-recipesfood.firebaseio.com/recipes.json',
-        recipes
-      )
-      .subscribe((resp) => {});
+    return this.http.put(
+      'https://recipe-book-13023-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+      recipes
+    );
   }
 
-  fetchRecipes(): any {
-    // Interceptor atack in here
+  fetchRecipes(): Observable<
+    {
+      ingredients: Ingredient[];
+      name: string;
+      description: string;
+      imagePath: string;
+    }[]
+  > {
     return this.http
       .get<Recipe[]>(
-        'https://ng-project-recipesfood.firebaseio.com/recipes.json'
+        'https://recipe-book-13023-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
       )
       .pipe(
         map((recipes) => {
