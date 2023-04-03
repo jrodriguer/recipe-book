05134f9aba99593a9d7e605cf041388e3fa0fed2
Julio <46267103+jrodriguer@users.noreply.github.com>
2023-04-03 07:55:29 +0200

    access env variables using the global variable process.env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyAUd8Fsm2ksLXrLtFWjB_fqCsTdDHABRvY',
-    authDomain: 'recipe-book-13023.firebaseapp.com',
-    projectId: 'recipe-book-13023',
-    storageBucket: 'recipe-book-13023.appspot.com',
-    messagingSenderId: '436106824501',
-    appId: '1:436106824501:web:fc24243dd3aac55a4a1a7c'
+    apiKey: process.env["API_KEY"],
+    authDomain: process.env["AUTH_DOMAIN"],
+    projectId: process.env["PROJECT_ID"],
+    storageBucket: process.env["STORAGE_BUCKET"],
+    messagingSenderId: process.env["MESSAGING_SENDER_ID"],
+    appId: process.env["ID"]
   }
 };
