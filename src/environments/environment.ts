6eea465ff3c00f6b65880b18cbafae690771ccd2
Julio <46267103+jrodriguer@users.noreply.github.com>
2023-06-04 17:51:43 +0200

    remove old firebase environment properties

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,12 +10,4 @@ export const environment = {
   },
   production: false,
   useEmulators: true,
-  firebaseConfig: {
-    apiKey: 'AIzaSyAUd8Fsm2ksLXrLtFWjB_fqCsTdDHABRvY',
-    authDomain: 'recipe-book-13023.firebaseapp.com',
-    projectId: 'recipe-book-13023',
-    storageBucket: 'recipe-book-13023.appspot.com',
-    messagingSenderId: '436106824501',
-    appId: '1:436106824501:web:fc24243dd3aac55a4a1a7c'
-  }
 };
